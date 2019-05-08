33619c085f9b98a7c3cb7f5ac30e3fb4d0f5da38
Elnakova Lubov <elannakova@gmail.com>
2019-05-08 15:40:58 -0700

    added reading data from input

--- src/simpleSearchEngine/Main.java
+++ src/simpleSearchEngine/Main.java
@@ -1,7 +1,31 @@
 package simpleSearchEngine;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+    public static void main(String args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of people:");
+        int peopleNumber = sc.nextInt();
+
+        String[] peopleArray = new String[peopleNumber];
+
+        System.out.println("Enter all people:");
+
+        for (int i = 0; i < peopleNumber; peopleNumber++) {
+            peopleArray[i] = sc.nextLine();
+        }
+
+        System.out.println("Enter the number of search queries:");
+        int queriesNumber = sc.nextInt();
+
+        for (int i = 0; i < queriesNumber; i++) {
+            System.out.println("Enter data to search people:");
+            String searchLine = sc.nextLine();
+
+        }
+
     }
 }
